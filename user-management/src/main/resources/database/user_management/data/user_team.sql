30f079f6e86e201b26f0ea136a25d6289bfe7c3d
kimhieu-dev <nguyenkimhieu.dev@gmail.com>
2026-03-22 10:26:59 +0700

    update sql

---
+++ user-management/src/main/resources/database/user_management/data/user_team.sql
@@ -0,0 +1,7 @@
+INSERT INTO user_management.user_team (id, user_id, team_id) VALUES (8, 1, 27);
+INSERT INTO user_management.user_team (id, user_id, team_id) VALUES (9, 1, 28);
+INSERT INTO user_management.user_team (id, user_id, team_id) VALUES (10, 1, 29);
+INSERT INTO user_management.user_team (id, user_id, team_id) VALUES (11, 2, 30);
+INSERT INTO user_management.user_team (id, user_id, team_id) VALUES (12, 2, 31);
+INSERT INTO user_management.user_team (id, user_id, team_id) VALUES (13, 6, 32);
+INSERT INTO user_management.user_team (id, user_id, team_id) VALUES (14, 6, 33);
